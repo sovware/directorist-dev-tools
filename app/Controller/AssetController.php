22ab441d035e2d21a463205ac3519563f97e2138
Syed Galib Ahmed <syedgalibahmed@gmail.com>
2021-07-09 17:42:11 +0600

    added global script atts override feature, improved

--- app/Controller/AssetController.php
+++ app/Controller/AssetController.php
@@ -3,7 +3,6 @@
 namespace DirectoristDevToolsAPP\Controller;
 
 class AssetController {
-
     public $css_scrips = [];
     public $js_scrips  = [];
     
@@ -14,18 +13,21 @@ class AssetController {
 
         add_filter( 'directorist_js_scripts', [ $this, 'update_js_scripts' ] );
         add_filter( 'directorist_css_scripts', [ $this, 'update_css_scripts' ] );
+
+        add_filter( 'directorist_css_scripts', [ $this, 'update_global_atts_of_css_scripts' ] );
+        add_filter( 'directorist_css_scripts', [ $this, 'update_global_atts_of_js_scripts' ] );
     }
 
     // load_css_scripts
     public function load_css_scripts() {
-        $new_css_scripts = [];
+        $new_css_scripts = apply_filters( 'directorist_dev_tools_css_scripts', [] );
 
         $this->css_scrips = $new_css_scripts;
     }
 
     // load_js_scripts
     public function load_js_scripts() {
-        $new_js_scripts = [];
+        $new_js_scripts = apply_filters( 'directorist_dev_tools_js_scripts', [] );;
 
         $this->js_scrips = $new_js_scripts;
     }
@@ -38,8 +40,36 @@ class AssetController {
 
     // update_js_scripts
     public function update_js_scripts( $scripts = [] ) {
-        
+
         return self::merge_scripts( $scripts, $this->js_scrips );
+    }
+
+    // update_global_atts_of_css_scripts
+    public function update_global_atts_of_css_scripts( $scripts = [] ) {
+        $global_atts = OptionController::get_option('css_global_atts');
+
+        $global_atts = ( ! empty( $global_atts ) && is_array( $global_atts ) ) ? $global_atts : [];
+        $updated_scripts = $scripts;
+
+        foreach ( $updated_scripts as $script_key => $script_args ) {
+            $updated_scripts[ $script_key ] = array_merge( $script_args, $global_atts );
+        }
+
+        return $updated_scripts;
+    }
+
+    // update_global_atts_of_js_scripts
+    public function update_global_atts_of_js_scripts( $scripts = [] ) {
+        $global_atts = OptionController::get_option('js_global_atts');
+
+        $global_atts = ( ! empty( $global_atts ) && is_array( $global_atts ) ) ? $global_atts : [];
+        $updated_scripts = $scripts;
+
+        foreach ( $updated_scripts as $script_key => $script_args ) {
+            $updated_scripts[ $script_key ] = array_merge( $script_args, $global_atts );
+        }
+
+        return $updated_scripts;
     }
 
     // merge_scripts
---
+++ app/Controller/OptionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DirectoristDevToolsAPP\Controller;
+
+class OptionController {
+    public static $default_options = [];
+
+    // setup_default_options
+    public static function setup_default_options() {
+        $default_options = [
+            'css_global_atts' => [],
+            'js_global_atts'  => [],
+        ];
+
+        self::$default_options = apply_filters( 'directorist_dev_tools_default_options', $default_options );
+    }
+
+    // get_option
+    public static function get_option( $option_key = '' ) {
+        self::setup_default_options();
+
+        return get_option( '_ddt_' . $option_key, self::$default_options[ $option_key ], true );
+    }
+
+    // update_option
+    public static function update_option( $option_key = '', $value = '' ) {
+        
+        return update_option( '_ddt_' . $option_key, $value );
+    }
+
+    // delete_option
+    public static function delete_option( $option_key = '' ) {
+        
+        return delete_option( '_ddt_' . $option_key );
+    }
+}
